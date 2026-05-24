e27041ed26ab58741fc64312cf91e725deecad33
shxzz <shxzz11wake@qq.com>
2026-05-24 23:39:26 +0800

    build: prefer official Maven repositories

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,10 +1,10 @@
 allprojects {
     repositories {
+        google()
+        mavenCentral()
         maven { url = uri("https://maven.aliyun.com/repository/google") }
         maven { url = uri("https://maven.aliyun.com/repository/central") }
         maven { url = uri("https://maven.aliyun.com/repository/public") }
-        google()
-        mavenCentral()
     }
 }
 
